0ef501547398abfa31423a4eae8f5bd3ea0d39bc
MicrobitAnne <161485257+MicrobitAnne@users.noreply.github.com>
2024-03-11 11:30:41 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    if (true) {
-        basic.showNumber(museum)
-        museum += 1
-    } else {
-        basic.showNumber(0)
-        museum += museum - 1
-    }
+    basic.showNumber(museum)
+    museum += -1
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(museum)
+    museum += 1
 })
 let museum = 0
 museum = 0
