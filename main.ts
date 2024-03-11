8725f62223d91dbc590b02aa6e865dc4376c27ff
MicrobitAnne <161485257+MicrobitAnne@users.noreply.github.com>
2024-03-11 11:14:59 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    if (true) {
+        basic.showNumber(museum)
+        museum += 1
+    } else {
+        basic.showNumber(0)
+        museum += museum - 1
+    }
+})
+let museum = 0
+museum = 0
